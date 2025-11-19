8b93c9bc217b94f467dbf852119f228068c4273a
Victor Vianna <victorvianna44@gmail.com>
2025-11-19 13:26:11 -0300

    atualizacao layout e configs do clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,53 @@
+// src/app/layout.tsx
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+import { TRPCReactProvider } from "~/trpc/react";
+import { Toaster } from "~/components/ui/toaster";
+import type { Metadata } from "next";
 import "~/styles/globals.css";
-import { GeistSans } from "geist/font/sans";
-import { ClientWrapper } from "~/components/ClientWrapper";
 
-export const metadata = {
-  title: "Igor Barber",
-  description: "Sistema de Agendamento",
+export const metadata: Metadata = {
+  title: "Barbershop",
+  description: "Sistema de agendamentos da barbearia",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <html lang="pt-BR" className={GeistSans.variable}>
-      <body className="min-h-screen flex flex-col ">
-        <ClientWrapper>{children}</ClientWrapper>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="pt-BR">
+        <body>
+          {/* Header direto no layout */}
+          <header className="flex h-16 items-center justify-end gap-4 bg-gradient-to-br from-zinc-900 to-zinc-800 p-4 text-white">
+            <SignedOut>
+              <SignInButton />
+              <SignUpButton>
+                <button className="h-10 rounded-full bg-[#6c47ff] px-4 text-sm font-medium text-white sm:h-12 sm:px-5 sm:text-base">
+                  Sign Up
+                </button>
+              </SignUpButton>
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </header>
+
+          {/* Conteúdo envolvido pelo TRPCReactProvider */}
+          <TRPCReactProvider>{children}</TRPCReactProvider>
+
+          {/* Toast global */}
+          <Toaster />
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
